b54c989cd3256f8303563591c54b7f946e51b3c4
AMMI <AMMI@AMMI-PC>
2018-10-06 16:11:52 +0530

    First Commit

---
+++ Sample_Maven_Project/src/test/java/Sample_Maven_Project/Sample_Maven_Project/Sample.java
@@ -0,0 +1,12 @@
+package Sample_Maven_Project.Sample_Maven_Project;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+	@Test
+	public void SampleTest()
+	{
+		System.out.println("Sample");
+	}
+
+}
